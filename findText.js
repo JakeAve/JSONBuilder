6d9fa850d04508569b92e70a447a99d1cabbb501
JakeAve <jacobjavery@gmail.com>
2019-04-19 21:46:53 -0600

    created find bar

--- findText.js
+++ findText.js
@@ -1,30 +1,106 @@
 const { ipcRenderer } = require('electron')
 
-
-const findContainer = document.createElement('div');
-findContainer.classList.add('find-container', 'hidden');
-findContainer.innerHTML = `
+const findBar = document.createElement('div');
+findBar.classList.add('find-bar', 'hidden');
+findBar.innerHTML = `
     <input id="find-input">
     <div id="matches">0/0</div>
-    <button>&lt;</button>
-    <button>&gt;</button>
-    <button id="hide-find">x</button>
+    <button title="Previous">&lt;</button>
+    <button title="Next">&gt;</button>
+    <button id="hide-find" title="Close find bar">x</button>
 `;
 
-
-document.body.appendChild(findContainer);
+document.body.appendChild(findBar);
 
 function showFind() {
-    findContainer.classList.remove('hidden');
-    findContainer.querySelector('#find-input').focus();
+    findBar.classList.remove('hidden');
+    findBar.querySelector('#find-input').focus();
+    findBar.querySelector('#find-input').select();
 }
 
-document.querySelector('#hide-find').addEventListener('click', () => {
-    findContainer.classList.add('hidden');
-    findText('');
-})
+const allMatches = {
+    fullArray : [],
 
-document.querySelector('#find-input').addEventListener('keyup', (e) => findText(e.target.value))
+    currentIndex : 0,
+
+    get selectedMatch() {
+        return this.fullArray[this.currentIndex] || null
+    },
+
+    earlierMatch : null,
+    set earlierMatchIndex(index) {
+        this.earlierMatch = this.fullArray[index];
+    },
+
+    //creates an array of all the matches
+    getMatches : function () {
+        const markTags = document.querySelector('table').querySelectorAll('mark');
+        const elements = Array.from(markTags);
+        if (elements.length) {
+            const matches = elements.map((el, index) => {
+                return {
+                    ['element'] : el, 
+                    index, 
+                    ['pos'] :el.getBoundingClientRect().top
+                }
+            })
+            this.fullArray = matches;
+            this.identifyTopMatch();
+        } else 
+            this.resetFindBar();
+    },
+
+    //finds and selects the match that is closest to the top of the viewport
+    identifyTopMatch : function () {
+        if (this.fullArray.length) {
+            const matches = this.fullArray;
+            let topMatch = matches[0];
+            let index = 0;
+            while (topMatch.pos < 0 && index < this.fullArray.length - 1) {
+                topMatch = matches[index ++]
+            }
+            this.currentIndex = index === 0 ? 0 : index --;
+            this.jumpToMatch();
+        }
+    },
+
+    updateDisplay : function () {
+        document.querySelector('#matches').innerHTML = `${this.selectedMatch ? this.currentIndex + 1 : 0} / ${this.fullArray.length}`;
+    },
+
+    nextMatch : function () {
+        if (this.currentIndex < this.fullArray.length - 1)
+            this.currentIndex ++;
+        else
+            this.currentIndex = 0;
+        this.jumpToMatch();
+    },
+    previousMatch : function () {
+        if (this.currentIndex > 0)
+            this.currentIndex --;
+        else 
+            this.currentIndex = this.fullArray.length - 1;
+        this.jumpToMatch();
+    },
+
+    //scrolls the match into view and handles the selected coloring
+    jumpToMatch : function () {
+        if (this.selectedMatch) {
+            this.selectedMatch.element.scrollIntoView();
+            window.scrollBy(0, -100);
+            this.selectedMatch.element.classList.add('selected-mark');
+            this.earlierMatch ? this.earlierMatch.element.classList.remove('selected-mark') : null;
+            this.earlierMatchIndex = this.currentIndex;
+        }
+        this.updateDisplay();
+    },
+
+    resetFindBar : function () {
+        allMatches.fullArray = [];
+        allMatches.currentIndex = 0;
+        allMatches.updateDisplay();
+    }
+}
 
 function findText(value) {
     const patt = `(${value})`
@@ -35,43 +111,46 @@ function findText(value) {
             cell.innerHTML = cell.textContent.replace(re, (wholeMatch, firstMatch) => {
                 return `<mark>${firstMatch}</mark>`
             })
-        else  cell.innerHTML = cell.textContent.replace(/<\/?mark>/g, '')
+        //when there is no text, it gets rid of all mark tags
+        else  cell.innerHTML = cell.textContent.replace(/<\/?mark.*?>/g, '')
     }
     if (value) 
-        getMatches();
+        allMatches.getMatches();
+    else 
+        allMatches.resetFindBar();
 }
 
-function getMatches() {
-    const markTags = document.querySelector('table').querySelectorAll('mark');
-    const elements = Array.from(markTags);
-    const matches = elements.map((el, index) => {
-        return {
-            ['element'] : el, 
-            index, 
-            ['pos'] :el.getBoundingClientRect().top
-            }
-    })
+document.querySelector('#hide-find').addEventListener('click', () => {
+    findBar.classList.add('hidden');
+    findText('');
+})
 
-    return matches;
+const map = {}; // for key presses
+document.querySelector('#find-input').addEventListener('keyup', (e) => {
+    if (e.keyCode === 13 || e.keyCode === 16) { //ENTER or SHIFT
+        onkeydown = onkeyup = (e) => {
+            map[e.keyCode] = e.type == 'keydown';
+            /* insert conditional here */
+            if (map[13] && !map[16]) //ENTER
+                allMatches.nextMatch()
+            if (map[13] && map[16]) //SHIFT + ENTER
+                allMatches.previousMatch()
+        } 
+    } else findText(e.target.value)
 
-    console.log(matches);
-    function getTopMatch() {
-        let topMatch = matches[0];
-        let index = 0;
-        while (topMatch.pos < 0) {
-            topMatch = matches[index ++]
-        }
-        return topMatch;
-    }
-    const topMatch = matches.length ? getTopMatch() : null;
+})
 
-    document.querySelector('#matches').innerHTML = `${topMatch ? topMatch.index + 1 : 0} / ${matches.length}`;
-}
+const arrowBtns = findBar.querySelectorAll('button:not(last-of-type)');
 
+arrowBtns[0].addEventListener('click', () => {
+    allMatches.previousMatch()
+})
 
+arrowBtns[1].addEventListener('click', () => {
+    allMatches.nextMatch()
+})
 
 ipcRenderer.on('find', showFind);
-
 
 document.addEventListener('dom-ready', () => {
     setUpFind();
